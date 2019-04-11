2883c3460045e408e386ab8863c4e2752f15b183
Mick Piereder <mick.io@protonmail.com>
2019-04-11 16:47:57 -0600

    finished shapeArea

--- edge_of_the_ocean.go
+++ edge_of_the_ocean.go
@@ -1,6 +1,7 @@
 package codesignal
 
-/*
+/* adjacentElementsProduct
+
 Given an array of integers, find the pair of adjacent elements that has the
 largest product and return that product.
 
@@ -37,3 +38,71 @@ func adjacentElementsProduct(input []int) (largest int) {
 	}
 	return largest
 }
+
+/* shapeArea
+
+Below we will define an n-interesting polygon. Your task is to find the area
+of a polygon for a given n.
+
+A 1-interesting polygon is just a square with a side of length 1. An
+n-interesting polygon is obtained by taking the n - 1-interesting polygon and
+appending 1-interesting polygons to its rim, side by side. You can see the 1-,
+2-, 3- and 4-interesting polygons in the picture below.
+
+Example
+
+    For n = 2, the output should be
+    shapeArea(n) = 5;
+    For n = 3, the output should be
+    shapeArea(n) = 13.
+
+Input/Output
+
+    [execution time limit] 4 seconds (go)
+
+    [input] integer n
+
+    Guaranteed constraints:
+    1 ≤ n < 104.
+
+    [output] integer
+        The area of the n-interesting polygon.
+*/
+
+func shapeArea(n int) int {
+	return n*n + (n-1)*(n-1)
+}
+
+/* Make Array Consecutive 2
+
+Ratiorg got statues of different sizes as a present from CodeMaster for his
+birthday, each statue having an non-negative integer size. Since he likes to
+make things perfect, he wants to arrange them from smallest to largest so that
+each statue will be bigger than the previous one exactly by 1. He may need some
+additional statues to be able to accomplish that. Help him figure out the
+minimum number of additional statues needed.
+
+Example
+
+For statues = [6, 2, 3, 8], the output should be
+makeArrayConsecutive2(statues) = 3.
+
+Ratiorg needs statues of sizes 4, 5 and 7.
+
+Input/Output
+
+    [execution time limit] 4 seconds (go)
+
+    [input] array.integer statues
+
+    An array of distinct non-negative integers.
+
+    Guaranteed constraints:
+    1 ≤ statues.length ≤ 10,
+    0 ≤ statues[i] ≤ 20.
+
+    [output] integer
+		The minimal number of statues that need to be added to existing statues
+		such that it contains every integer size from an interval [L, R]
+		(for some L, R) and no other sizes.
+*/
--- edge_of_the_ocean_test.go
+++ edge_of_the_ocean_test.go
@@ -20,3 +20,20 @@ func Test_adjacentElementProduct(t *testing.T) {
 		}
 	}
 }
+
+func Test_shapeArea(t *testing.T) {
+	tests := []struct{ input, expect int }{
+		{2, 5},
+		{3, 13},
+		{1, 1},
+		{5, 41},
+		{7000, 97986001},
+	}
+	for _, test := range tests {
+		output := shapeArea(test.input)
+		if output != test.expect {
+			msg := "\n[FAIL] shapeArea(%d) == %d expect %d"
+			t.Errorf(msg, test.input, output, test.expect)
+		}
+	}
+}
