5dfe240f634185962836df8b142ab2032a23037f
Mick Piereder <mick.io@protonmail.com>
2019-04-11 16:26:56 -0600

    added additional comment information

---
+++ edge_of_the_ocean_test.go
@@ -0,0 +1,22 @@
+package codesignal
+
+import "testing"
+
+func Test_adjacentElementProduct(t *testing.T) {
+	tests := []struct {
+		input  []int
+		expect int
+	}{
+		{[]int{1, 2, 3, 4, 5}, 20},
+		{[]int{3, 6, -2, -5, 7, 3}, 21},
+		{[]int{-1, -2}, 2},
+		{[]int{5, 1, 2, 3, 1, 4}, 6},
+	}
+	for _, test := range tests {
+		output := adjacentElementsProduct(test.input)
+		if output != test.expect {
+			msg := "\n[FAIL] adjacentElementsProduct(%s) == %d expect %d"
+			t.Errorf(msg, test.input, output, test.expect)
+		}
+	}
+}
