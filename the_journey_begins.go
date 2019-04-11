275610af7e4f41d6e15b3747193d57a658871658
Mick Piereder <mick.io@protonmail.com>
2019-04-11 16:04:56 -0600

    added comments

--- the_journey_begins.go
+++ the_journey_begins.go
@@ -2,13 +2,21 @@ package codesignal
 
 import "math"
 
-func add(param1 int, param2 int) int {
-	return param1 + param2
+// Write a function that returns the sum of two numbers.
+
+func add(n1 int, n2 int) int {
+	return n1 + n2
 }
+
+// Given a year, return the century it is in. The first century spans from the
+// year 1 up to and including the year 100, the second - from the year 101 up
+// to and including the year 200, etc.
 
 func centuryFromYear(year int) int {
 	return int(math.Ceil(float64(year) / 100))
 }
+
+// Given the string, check if it is a palindrome.
 
 func checkPalindrome(s string) bool {
 	chars := []rune(s)
