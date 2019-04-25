168567c58abca5c2d54d197d0f9877c6e480ea4d
Mick Piereder <mick.io@protonmail.com>
2019-04-25 12:24:48 -0600

    added more unit test

--- arcade/intro/7-almost-increasing-sequence/almost-increasing-sequence_test.go
+++ arcade/intro/7-almost-increasing-sequence/almost-increasing-sequence_test.go
@@ -9,16 +9,25 @@ func Test_almostIncreaseSequence(t *testing.T) {
 		input  []int
 		expect bool
 	}{
-		{[]int{1, 2, 1, 2}, false},
-		{[]int{3, 6, 5, 8, 10, 20, 15}, false},
 		{[]int{1, 3, 2, 1}, false},
 		{[]int{1, 3, 2}, true},
+		{[]int{1, 2, 1, 2}, false},
+		{[]int{3, 6, 5, 8, 10, 20, 15}, false},
 		{[]int{1, 1, 2, 3, 4, 4}, false},
 		{[]int{1, 4, 10, 4, 2}, false},
 		{[]int{10, 1, 2, 3, 4, 5}, true},
-		{[]int{1, 2, 3, 4, 3, 6}, true},
+		{[]int{1, 1, 1, 2, 3}, false},
+		{[]int{0, -2, 5, 6}, true},
+		{[]int{1, 2, 3, 4, 5, 3, 5, 6}, false},
+		{[]int{40, 50, 60, 10, 20, 30}, false},
+		{[]int{1, 1}, true},
 		{[]int{1, 2, 5, 3, 5}, true},
+		{[]int{1, 2, 5, 5, 5}, false},
+		{[]int{10, 1, 2, 3, 4, 5, 6, 1}, false},
+		{[]int{1, 2, 3, 4, 3, 6}, true},
 		{[]int{1, 2, 3, 4, 99, 5, 6}, true},
+		{[]int{123, -17, -5, 1, 2, 3, 12, 43, 45}, true},
+		{[]int{3, 5, 67, 98, 3}, true},
 	}
 	for _, test := range tests {
 		original := make([]int, len(test.input))
